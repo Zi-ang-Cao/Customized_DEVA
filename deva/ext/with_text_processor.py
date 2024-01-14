2db2b47b420e57a9ce02e024cdba6dbd7c771c97
Zi-ang Cao <ziangcao@stanford.edu>
2024-01-14 02:10:24 -0800

    customized refined_processor_with_text()

--- deva/ext/with_text_processor.py
+++ deva/ext/with_text_processor.py
@@ -18,6 +18,8 @@ except ImportError:
     from GroundingDINO.groundingdino.util.inference import Model as GroundingDINOModel
 from segment_anything import SamPredictor
 
+import torch.nn.functional as F
+
 
 def make_segmentation_with_text(cfg: Dict, image_np: np.ndarray, gd_model: GroundingDINOModel,
                                 sam_model: SamPredictor, prompts: List[str],
@@ -52,6 +54,10 @@ def process_frame_with_text(deva: DEVAInferenceCore,
         'frame': [frame_name],
         'shape': [h, w],
     })
+
+
+    parsed_img = None
+
 
     if cfg['temporal_setting'] == 'semionline':
         if ti + cfg['num_voting_frames'] > deva.next_voting_frame:
@@ -105,9 +111,11 @@ def process_frame_with_text(deva: DEVAInferenceCore,
                                    prompts=prompts)
 
     elif cfg['temporal_setting'] == 'online':
+        # FOR MM_LFD, USE THIS Branch ONLY!!!!
+
         if ti % cfg['detection_every'] == 0:
             # incorporate new detections
-            mask, segments_info = make_segmentation_with_text(cfg, image_np, gd_model, sam_model,
+            mask, segments_info, parsed_img = make_segmentation_with_text(cfg, image_np, gd_model, sam_model,
                                                               prompts, new_min_side)
             frame_info.segments_info = segments_info
             prob = deva.incorporate_detection(image, mask, segments_info)
@@ -120,3 +128,75 @@ def process_frame_with_text(deva: DEVAInferenceCore,
                                shape=(h, w),
                                image_np=image_np,
                                prompts=prompts)
+        
+
+        return parsed_img
+
+
+
+
+
+@torch.inference_mode()
+def refined_processor_with_text(deva: DEVAInferenceCore,
+                            gd_model: GroundingDINOModel,
+                            sam_model: SamPredictor,
+                            ti: int,
+                            image_np: np.ndarray = None,
+                            obj_ids = None,
+                            ) -> None:
+    
+    # image_np, if given, should be in RGB
+    assert image_np is not None
+
+
+    cfg = deva.config
+    raw_prompt = cfg['prompt']
+    prompts = raw_prompt.split('.')
+
+    h, w = image_np.shape[:2]
+    new_min_side = cfg['size']
+    need_resize = new_min_side > 0
+    image = get_input_frame_for_deva(image_np, new_min_side)
+
+    frame_info = FrameInfo(image, None, None, ti, {
+        'frame': ["refined_processor_with_text_NULL_FRAME_NAME"],
+        'shape': [h, w],
+    })
+
+
+    if cfg['temporal_setting'] == 'semionline':
+        raise NotImplementedError
+
+    elif cfg['temporal_setting'] == 'online':
+        # FOR MM_LFD, USE THIS Branch ONLY!!!!
+
+        if ti % cfg['detection_every'] == 0:
+            # incorporate new detections
+            mask, segments_info = make_segmentation_with_text(cfg, image_np, gd_model, sam_model,
+                                                              prompts, new_min_side)
+            frame_info.segments_info = segments_info
+            prob = deva.incorporate_detection(image, mask, segments_info)
+
+            # seg = parsed_img
+        else:
+            # Run the model on this frame
+            prob = deva.step(image, None, None)
+
+        shape=(h, w)
+
+        if need_resize:
+            prob = F.interpolate(prob.unsqueeze(1), shape, mode='bilinear', align_corners=False)[:,
+                                                                                                0]
+        
+        # Probability mask -> index mask
+        index_mask = torch.argmax(prob, dim=0)    # (H, W), either 0 or 1 - n (for n objects)
+        index_mask = index_mask.cpu().numpy()
+        
+        if obj_ids is None:
+            # cared_prompt_ids = list(range(len(prompts)))
+            obj_ids = np.unique(index_mask)[1:]    # 0 is the background
+            
+        seg = np.isin(index_mask, obj_ids).astype(np.uint8) * 255
+        seg = np.repeat(seg[:, :, None], 3, axis=-1)
+        
+        return seg
